47ea8500b15b46874b892743affc9ef53e7d4bc6
salty-max <max@jellycat.fr>
2022-05-15 20:08:08 +0200

    Styles radio inputs and fix capitalize function

--- src/App.tsx
+++ src/App.tsx
@@ -27,8 +27,9 @@ const CasesContainer = styled.div`
 
 const CaseContainer = styled.div`
   &:not(:last-of-type) {
-    margin-right: 0.5rem;
+    margin-right: 1rem;
   }
+  position: relative;
 `;
 
 const Grid = styled.div`
@@ -73,10 +74,62 @@ const Button = styled.button`
   }
 `;
 
+const Radio = styled.input`
+  &:checked,
+  &:not(:checked) {
+    position: absolute;
+    left: -9999px;
+  }
+  &:checked + label,
+  &:not(:checked) + label {
+    position: relative;
+    padding-left: 28px;
+    cursor: pointer;
+    line-height: 20px;
+    display: inline-block;
+    color: #666;
+  }
+  &:checked + label:before,
+  &:not(:checked) + label:before {
+    content: "";
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 18px;
+    height: 18px;
+    border: 1px solid #ddd;
+    border-radius: 100%;
+    background: #fff;
+  }
+  &:checked + label:after,
+  &:not(:checked) + label:after {
+    content: "";
+    width: 12px;
+    height: 12px;
+    background: #9c88ff;
+    position: absolute;
+    top: 4px;
+    left: 4px;
+    border-radius: 100%;
+    -webkit-transition: all 0.2s ease;
+    transition: all 0.2s ease;
+  }
+  &:not(:checked) + label:after {
+    opacity: 0;
+    -webkit-transform: scale(0);
+    transform: scale(0);
+  }
+  &:checked + label:after {
+    opacity: 1;
+    -webkit-transform: scale(1);
+    transform: scale(1);
+  }
+`;
+
 const toAllCaps = (s: string): string =>
   s
     .split(" ")
-    .map((w) => `${w.charAt(0).toUpperCase()}${w.slice(1)}`)
+    .map((w) => `${w.charAt(0).toUpperCase()}${w.substring(1).toLowerCase()}`)
     .join(" ");
 
 const App: React.FC = () => {
@@ -98,7 +151,9 @@ const App: React.FC = () => {
         setResult(item.toUpperCase());
         break;
       case CaseChoice.Capitalize:
-        setResult(`${item.charAt(0).toUpperCase()}${item.slice(1)}`);
+        setResult(
+          `${item.charAt(0).toUpperCase()}${item.substring(1).toLowerCase()}`,
+        );
         break;
       case CaseChoice.AllCaps:
         setResult(toAllCaps(item));
@@ -118,40 +173,40 @@ const App: React.FC = () => {
         <FormProvider {...form}>
           <CasesContainer>
             <CaseContainer>
-              <label htmlFor="lower">Lowercase</label>
-              <input
+              <Radio
                 type="radio"
                 id="lower"
                 {...register("caseChoice")}
                 value={CaseChoice.Lower}
               />
+              <label htmlFor="lower">Lowercase</label>
             </CaseContainer>
             <CaseContainer>
-              <label htmlFor="upper">Uppercase</label>
-              <input
+              <Radio
                 type="radio"
                 id="upper"
                 {...register("caseChoice")}
                 value={CaseChoice.Upper}
               />
+              <label htmlFor="upper">Uppercase</label>
             </CaseContainer>
             <CaseContainer>
-              <label htmlFor="capitalize">Capitalize</label>
-              <input
+              <Radio
                 type="radio"
                 id="capitalize"
                 {...register("caseChoice")}
                 value={CaseChoice.Capitalize}
               />
+              <label htmlFor="capitalize">Capitalize</label>
             </CaseContainer>
             <CaseContainer>
-              <label htmlFor="all_caps">Capitalize all</label>
-              <input
+              <Radio
                 type="radio"
                 id="all_caps"
                 {...register("caseChoice")}
                 value={CaseChoice.AllCaps}
               />
+              <label htmlFor="all_caps">Capitalize all</label>
             </CaseContainer>
           </CasesContainer>
           <Grid>
